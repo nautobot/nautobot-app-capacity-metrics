d069cc94e8a91b7abae02469a5c08894716b0393
nniehoff <github@nickniehoff.net>
2021-07-23 12:13:20 -0600

    Adding Prometheus and Grafana

--- development/nautobot_config.py
+++ development/nautobot_config.py
@@ -248,12 +248,24 @@ PLUGINS = ["nautobot_capacity_metrics"]
 
 # Plugins configuration settings. These settings are used by various plugins that the user may have installed.
 # Each key in the dictionary is the name of an installed plugin and its value is a dictionary of settings.
-# PLUGINS_CONFIG = {
-#     'my_plugin': {
-#         'foo': 'bar',
-#         'buzz': 'bazz'
-#     }
-# }
+PLUGINS_CONFIG = {
+    "nautobot_capacity_metrics": {
+        "app_metrics": {
+            "models": {
+                "dcim": {
+                    "Site": True,
+                    "Rack": True,
+                    "Device": True,
+                    "Interface": True,
+                    "Cable": True,
+                },
+                "ipam": {"IPAddress": True, "Prefix": True},
+            },
+            "queues": True,
+            "jobs": True,
+        }
+    },
+}
 
 # When determining the primary IP address for a device, IPv6 is preferred over IPv4 by default. Set this to True to
 # prefer IPv4 instead.
