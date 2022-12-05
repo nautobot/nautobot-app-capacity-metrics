768cb735f7a36f0410e6366d1cdb024f8cd87c67
Leo Kirchner <leo.kirchner98@gmail.com>
2022-12-05 08:24:24 +0100

    Update Nautobot version in tasks.py.

--- tasks.py
+++ tasks.py
@@ -38,7 +38,7 @@ namespace = Collection("nautobot_capacity_metrics")
 namespace.configure(
     {
         "nautobot_capacity_metrics": {
-            "nautobot_ver": "1.2.11",
+            "nautobot_ver": "1.5.2",
             "project_name": "nautobot_capacity_metrics",
             "python_ver": "3.7",
             "local": False,
