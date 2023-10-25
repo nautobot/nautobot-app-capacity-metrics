f6d432640a38b056ed6bec5adaa696f8b17f6800
Jan Snasel <jan.snasel@networktocode.com>
2023-10-25 07:42:57 +0000

    Cookie initialy baked by NetworkToCode Cookie Drift Manager Tool
    Template:
    
    ```
    {
        "template": "https://github.com/nautobot/cookiecutter-nautobot-app.git",
        "dir": "nautobot-app",
        "ref": "develop",
        "path": null
    }
    ```
    
    Cookie:
    
    ```
    {
        "remote": "https://github.com/nautobot/nautobot-plugin-capacity-metrics.git",
        "path": "/opt/ntc/drift-manager/outputs/nautobot-plugin-capacity-metrics",
        "repository_path": "/opt/ntc/drift-manager/outputs/nautobot-plugin-capacity-metrics",
        "dir": "",
        "branch_prefix": "drift-manager",
        "context": {
            "codeowner_github_usernames": "@nautobot/po-tl-telemetry",
            "full_name": "Network to Code, LLC",
            "email": "opensource@networktocode.com",
            "github_org": "nautobot",
            "plugin_name": "nautobot_capacity_metrics",
            "verbose_name": "Metrics & Monitoring Extension Plugin",
            "plugin_slug": "nautobot-capacity-metrics",
            "project_slug": "nautobot-plugin-capacity-metrics",
            "repo_url": "https://github.com/nautobot/nautobot-plugin-capacity-metrics",
            "base_url": "capacity-metrics",
            "min_nautobot_version": "2.0.0",
            "max_nautobot_version": "2.9999",
            "camel_name": "NautobotCapacityMetrics",
            "project_short_description": "Plugin to improve the instrumentation of Nautobot and expose additional metrics (Application Metrics, RQ Worker).",
            "model_class_name": "None",
            "open_source_license": "Apache-2.0",
            "docs_base_url": "https://docs.nautobot.com",
            "docs_app_url": "https://docs.nautobot.com/projects/capacity-metrics/en/latest",
            "_template": "https://github.com/nautobot/cookiecutter-nautobot-app.git",
            "_output_dir": "/opt/ntc/drift-manager/outputs",
            "_repo_dir": "/home/jsn/.cookiecutters/cookiecutter-nautobot-app/nautobot-app",
            "_checkout": "develop"
        },
        "base_branch": "develop",
        "remote_name": "origin",
        "pull_request_strategy": "PullRequestStrategy.CREATE",
        "post_actions": [
            "PostAction.BLACK"
        ],
        "baked_commit_ref": "",
        "draft": true
    }
    ```
    
    CLI Arguments:
    
    ```
    {
        "cookie_dir": "",
        "input": false,
        "json_filename": "setup-cookie-capacity-metrics.json",
        "output_dir": "./outputs",
        "push": true,
        "template": "https://github.com/nautobot/cookiecutter-nautobot-app.git",
        "template_dir": "nautobot-app",
        "template_ref": "develop",
        "pull_request": null,
        "post_action": [
            "black"
        ],
        "disable_post_actions": false,
        "draft": true
    }
    ```

--- nautobot_capacity_metrics/__init__.py
+++ nautobot_capacity_metrics/__init__.py
@@ -1,61 +1,26 @@
 """Plugin declaration for nautobot_capacity_metrics."""
+# Metadata is inherited from Nautobot. If not including Nautobot in the environment, this should be added
+from importlib import metadata
 
-__version__ = "1.1.1"
+__version__ = metadata.version(__name__)
 
 from nautobot.extras.plugins import NautobotAppConfig
 
-# Registry of functions that can generate additional application metrics
-# All functions in the registry should take no argument and return an Iterator (or list) of prometheus Metric Object
-# The Registry can be populated from the configuration file or using register_metric_func()
-__REGISTRY__ = []
 
-
-def register_metric_func(func):
-    """Register an additional function to generate application metrics.
-
-    Args:
-        func: python function, taking no argument that return a list of Prometheus Metric Object
-    """
-    if not callable(func):
-        raise TypeError(
-            f"Trying to register a {type(func)} into the application metric registry, only function (callable) are supporter"
-        )
-
-    __REGISTRY__.append(func)
-
-
-class MetricsExtConfig(NautobotAppConfig):
+class NautobotCapacityMetricsConfig(NautobotAppConfig):
     """Plugin configuration for the nautobot_capacity_metrics plugin."""
 
     name = "nautobot_capacity_metrics"
-    verbose_name = "Data, Metrics, and Monitoring Prometheus Endpoints"
+    verbose_name = "Metrics & Monitoring Extension Plugin"
     version = __version__
     author = "Network to Code, LLC"
-    author_email = "opensource@networktocode.com"
-    description = "Lightweight Nautobot App to expose additional metrics as Prometheus endpoints. Includes exposing Nautobot object data and metrics that can be collected and later viewed in Visualization tools."
+    description = "Plugin to improve the instrumentation of Nautobot and expose additional metrics (Application Metrics, RQ Worker).."
     base_url = "capacity-metrics"
     required_settings = []
     min_version = "2.0.0"
-    max_version = "2.99"
-    default_settings = {
-        "app_metrics": {
-            "models": {
-                "dcim": {
-                    "Site": True,
-                    "Rack": True,
-                    "Device": True,
-                },
-                "ipam": {"IPAddress": True, "Prefix": True},
-            },
-            "jobs": True,
-            "queues": True,
-            "versions": {
-                "basic": False,
-                "plugins": False,
-            },
-        }
-    }
+    max_version = "2.9999"
+    default_settings = {}
     caching_config = {}
 
 
-config = MetricsExtConfig  # pylint:disable=invalid-name
+config = NautobotCapacityMetricsConfig  # pylint:disable=invalid-name
