15e9e466517d69c16ae938c98dd79af1c24c87b5
bakebot <info@networktocode.com>
2025-01-07 18:39:17 +0000

    Cookie updated by NetworkToCode Cookie Drift Manager Tool
    Template:
    
    ```
    {
        "template": "https://github.com/nautobot/cookiecutter-nautobot-app.git",
        "dir": "nautobot-app",
        "ref": "refs/tags/nautobot-app-v2.4.1",
        "path": null
    }
    ```
    
    Cookie:
    
    ```
    {
        "remote": "https://github.com/nautobot/nautobot-app-capacity-metrics.git",
        "path": "/tmp/tmpuqq4sx9a/nautobot-app-capacity-metrics",
        "repository_path": "/tmp/tmpuqq4sx9a/nautobot-app-capacity-metrics",
        "dir": "",
        "branch_prefix": "drift-manager",
        "context": {
            "codeowner_github_usernames": "@nautobot/po-tl-telemetry",
            "full_name": "Network to Code, LLC",
            "email": "opensource@networktocode.com",
            "github_org": "nautobot",
            "app_name": "nautobot_capacity_metrics",
            "verbose_name": "Metrics & Monitoring Extension App",
            "app_slug": "nautobot-capacity-metrics",
            "project_slug": "nautobot-app-capacity-metrics",
            "repo_url": "https://github.com/nautobot/nautobot-app-capacity-metrics",
            "base_url": "capacity-metrics",
            "min_nautobot_version": "2.0.0",
            "max_nautobot_version": "2.9999",
            "camel_name": "NautobotCapacityMetrics",
            "project_short_description": "App to improve the instrumentation of Nautobot and expose additional metrics (Application Metrics, RQ Worker).",
            "model_class_name": "None",
            "open_source_license": "Apache-2.0",
            "docs_base_url": "https://docs.nautobot.com",
            "docs_app_url": "https://docs.nautobot.com/projects/capacity-metrics/en/latest",
            "_template": "https://github.com/nautobot/cookiecutter-nautobot-app.git",
            "_output_dir": "/tmp/tmpuqq4sx9a",
            "_repo_dir": "/github/home/.cookiecutters/cookiecutter-nautobot-app/nautobot-app",
            "_checkout": "refs/tags/nautobot-app-v2.4.1"
        },
        "base_branch": "develop",
        "remote_name": "origin",
        "pull_request_strategy": "PullRequestStrategy.CREATE",
        "post_actions": [
            "PostAction.RUFF",
            "PostAction.POETRY"
        ],
        "baked_commit_ref": "796a56cbd8a329db67c126e00d44d1305c05dadb",
        "draft": false
    }
    ```
    
    CLI Arguments:
    
    ```
    {
        "cookie_dir": "",
        "input": false,
        "json_filename": "",
        "output_dir": "",
        "push": true,
        "template": "",
        "template_dir": "",
        "template_ref": "refs/tags/nautobot-app-v2.4.1",
        "pull_request": null,
        "post_action": [
            "ruff",
            "poetry"
        ],
        "disable_post_actions": true,
        "draft": false
    }
    ```

---
+++ nautobot_capacity_metrics/urls.py
@@ -0,0 +1,23 @@
+"""Django urlpatterns declaration for nautobot_capacity_metrics app."""
+
+from django.templatetags.static import static
+from django.urls import path
+from django.views.generic import RedirectView
+from nautobot.apps.urls import NautobotUIViewSetRouter
+
+
+# Uncomment the following line if you have views to import
+# from nautobot_capacity_metrics import views
+
+
+router = NautobotUIViewSetRouter()
+
+# Here is an example of how to register a viewset, you will want to replace views.NautobotCapacityMetricsUIViewSet with your viewset
+# router.register("nautobot_capacity_metrics", views.NautobotCapacityMetricsUIViewSet)
+
+
+urlpatterns = [
+    path("docs/", RedirectView.as_view(url=static("nautobot_capacity_metrics/docs/index.html")), name="docs"),
+]
+
+urlpatterns += router.urls
