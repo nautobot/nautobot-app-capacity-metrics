e0b3b7f09c07458fe7cfbab4d4a2823dab5328f5
Gary Snider <75227981+gsnider2195@users.noreply.github.com>
2025-05-05 20:39:13 -0700

    Apply suggestions from code review

--- nautobot_capacity_metrics/urls.py
+++ nautobot_capacity_metrics/urls.py
@@ -3,10 +3,8 @@
 from django.templatetags.static import static
 from django.urls import path
 from django.views.generic import RedirectView
-from nautobot.apps.urls import NautobotUIViewSetRouter
 
 app_name = "nautobot_capacity_metrics"
-router = NautobotUIViewSetRouter()
 
 urlpatterns = [
     path("docs/", RedirectView.as_view(url=static("nautobot_capacity_metrics/docs/index.html")), name="docs"),
