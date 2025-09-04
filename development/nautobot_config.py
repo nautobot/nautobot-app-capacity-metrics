e1ef3ae232de6fb90db3382865ed128c1cfc7dc0
Roberto Duarte <roberto.duarte@codilime.com>
2025-09-04 00:44:54 -0300

    test against TestModel without registering test_models

--- development/nautobot_config.py
+++ development/nautobot_config.py
@@ -122,7 +122,6 @@ if not _TESTING:
 # Enable installed Apps. Add the name of each App to the list.
 PLUGINS = [
     "nautobot_capacity_metrics",
-    "nautobot_capacity_metrics.test_models",
 ]
 
 # Apps configuration settings. These settings are used by various Apps that the user may have installed.
@@ -142,7 +141,7 @@ PLUGINS_CONFIG = {
                 },
                 "ipam": {"IPAddress": True, "Prefix": True},
                 "extras": {"GitRepository": True},
-                "test_models": {"_module": "nautobot_capacity_metrics", "TestModel": True},
+                "test_models": {"_module": "nautobot_capacity_metrics_test_models", "TestModel": True},
             },
             "queues": True,
             "versions": {
